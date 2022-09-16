4598dedd20afab22960c5fafacae4e0e97f5c331
Raphaël Tétreault <raphaeltetreault@hotmail.com>
2022-09-16 02:11:35 -0400

    Improve cardata IO, add LZ compression and decompression

--- src/gfz-cli/Options.cs
+++ src/gfz-cli/Options.cs
@@ -1,4 +1,8 @@
 ﻿using CommandLine;
+using GameCube.AmusementVision;
+using GameCube.GFZ;
+using GameCube.GFZ.LZ;
+using GameCube.GFZ.Stage;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Processing;
 using SixLabors.ImageSharp.Processing.Processors.Transforms;
@@ -28,10 +32,10 @@ namespace Manifold.GFZCLI
                 "Removes all extensions from input file. True by default.\n\tEx: \"spr.ci8.png\" -> \"spr.sprite\" ";
 
 
-            public const string ImportCarDataToTSV =
-                "Creates a TSV representing the values of cardata.bin";
-            public const string ExportCarDataFromTSV =
-                "Creates a TSV representing the values of cardata.bin";
+            public const string CarDataToTSV =
+                "Creates a TSV spreadsheet from the values of cardata.lz binary.";
+            public const string CarDataFromTSV =
+                "Creates a binary from the values of cardata.tsv spreadsheet.";
         }
 
 
@@ -52,16 +56,26 @@ namespace Manifold.GFZCLI
         public string SearchPattern { get; set; } = string.Empty;
 
 
-        [Option("cardata-bin-to-tsv", Required = false, HelpText = Help.ImportCarDataToTSV)]
+        [Option("cardata-bin-to-tsv", Required = false, HelpText = Help.CarDataToTSV)]
         public string CarDataBinPath { get; set; } = string.Empty;
 
-        [Option("cardata-tsv-to-bin", Required = false, HelpText = Help.ExportCarDataFromTSV)]
+        [Option("cardata-tsv-to-bin", Required = false, HelpText = Help.CarDataFromTSV)]
         public string CarDataTsvPath { get; set; } = string.Empty;
 
 
+        [Option("format", Required = false, HelpText = Help.CarDataFromTSV)]
+        public string SerializationFormat { get; set; } = string.Empty;
+
+
+        [Option("lzd", Required = false, HelpText = Help.CarDataFromTSV)]
+        public string LzDecompressTarget { get; set; } = string.Empty;
+        [Option("lzc", Required = false, HelpText = Help.CarDataFromTSV)]
+        public string LzCompressTarget { get; set; } = string.Empty;
 
 
         public SearchOption SearchOption => SearchSubdirectories ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+        public SerializeFormat SerializeFormat => GetSerializeFormat(SerializationFormat);
+        public GxGame AvGame => GetAvFormat(SerializeFormat);
 
 
         public void PrintState()
@@ -73,6 +87,30 @@ namespace Manifold.GFZCLI
             Console.WriteLine($"{nameof(SearchSubdirectories)}: {SearchSubdirectories}");
             Console.WriteLine($"{nameof(SearchOption)}: {SearchOption}");
             Console.WriteLine($"{nameof(SearchPattern)}: {SearchPattern}");
+        }
+
+
+        private static SerializeFormat GetSerializeFormat(string serializeFormat)
+        {
+            serializeFormat = serializeFormat.ToLower();
+            switch (serializeFormat)
+            {
+                case "ax":
+                    return SerializeFormat.AX;
+
+                case "gx":
+                default:
+                    return SerializeFormat.GX;
+            }
+        }
+        private static GxGame GetAvFormat(SerializeFormat serializeFormat)
+        {
+            switch (serializeFormat)
+            {
+                case SerializeFormat.AX: return GxGame.FZeroAX;
+                case SerializeFormat.GX: return GxGame.FZeroGX;
+                default: throw new ArgumentException();
+            }
         }
 
     }
--- src/gfz-cli/Program.cs
+++ src/gfz-cli/Program.cs
@@ -1,5 +1,6 @@
 ﻿using CommandLine;
 using GameCube.GFZ.CarData;
+using GameCube.GFZ.LZ;
 using Manifold;
 using Manifold.IO;
 using System;
@@ -25,44 +26,132 @@ namespace Manifold.GFZCLI
             }
             VerboseConsole.IsVerbose = options.Verbose;
 
-            // Everythign else from here
+            // Everything else from here
             CarDataToTSV(options);
             CarDataToBIN(options);
+            //
+            LzDecompress(options);
+            LzCompress(options);
         }
 
         public static void CarDataToTSV(Options options)
         {
-            if (string.IsNullOrEmpty(options.CarDataBinPath))
+            string filePath = options.CarDataBinPath;
+            if (string.IsNullOrEmpty(filePath))
                 return;
 
-            using (var reader = new EndianBinaryReader(File.OpenRead(options.CarDataBinPath), CarData.endianness))
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException($"File at path {filePath} does not exist.");
+
+            // Decompress LZ if not decompressed yet
+            string extension = Path.GetExtension(filePath);
+            if (!string.IsNullOrEmpty(extension))
+            {
+                // Save decompressed file
+                LzUtility.DecompressAvLzToDisk(filePath, true);
+                VerboseConsole.WriteLine($"Decompressed {filePath} for conversion.");
+                // Update path to decompressed file
+                string fileName = Path.GetFileNameWithoutExtension(filePath);
+                string fileDir = Path.GetDirectoryName(filePath);
+                filePath = Path.Combine(fileDir, fileName);
+            }
+
+            using (var reader = new EndianBinaryReader(File.OpenRead(filePath), CarData.endianness))
             {
                 var carData = new CarData();
                 reader.Read(ref carData);
 
-                string outputPath = options.CarDataBinPath + ".tsv";
+                string outputPath = filePath + ".tsv";
                 using (var writer = new StreamWriter(File.Create(outputPath)))
                 {
                     carData.Serialize(writer);
+                    Console.WriteLine($"Created file: {outputPath}");
                 }
             }
         }
-
         public static void CarDataToBIN(Options options)
         {
-            if (string.IsNullOrEmpty(options.CarDataTsvPath))
+            string filePath = options.CarDataTsvPath;
+            if (string.IsNullOrEmpty(filePath))
                 return;
 
-            using (var reader = new StreamReader(File.OpenRead(options.CarDataTsvPath)))
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException($"File at path {filePath} does not exist.");
+
+            // Open CarData
+            using (var reader = new StreamReader(File.OpenRead(filePath)))
             {
                 var carData = new CarData();
                 carData.Deserialize(reader);
 
-                string outputPath = options.CarDataTsvPath + ".bin";
+                // Save out 
+                string outputPath = filePath + ".bin";
                 using (var writer = new EndianBinaryWriter(File.Create(outputPath), CarData.endianness))
                 {
                     carData.Serialize(writer);
+                    Console.WriteLine($"Created file: {outputPath}");
                 }
+                LzUtility.CompressAvLz(outputPath, GameCube.AmusementVision.GxGame.FZeroGX);
+            }
+        }
+
+        public static void LzDecompress(Options options)
+        {
+            string path = options.LzDecompressTarget;
+            if (string.IsNullOrEmpty(path))
+                return;
+
+            bool fileExists = File.Exists(path);
+            bool dirExists = Directory.Exists(path);
+            if (!fileExists && !dirExists)
+                throw new ArgumentException($"Provided file or folder path target {path} does not exist.");
+
+            if (fileExists)
+            {
+                LzUtility.DecompressAvLzToDisk(path, true);
+                Console.WriteLine($"Decompressed: {path}");
+            }
+            else if (dirExists)
+            {
+                var searchOption = options.SearchSubdirectories ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+                var enumerable = LzUtility.DecompressAvLzDirectoryToDisk(path, true, searchOption);
+                foreach (var file in enumerable)
+                    Console.WriteLine($"Decompressed: {file.filePath}");
+            }
+            else
+            {
+                Assert.IsTrue(false, "This code should never run.");
+            }
+        }
+        public static void LzCompress(Options options)
+        {
+            string path = options.LzCompressTarget;
+            if (string.IsNullOrEmpty(path))
+                return;
+
+            bool fileExists = File.Exists(path);
+            bool dirExists = Directory.Exists(path);
+            if (!fileExists && !dirExists)
+                throw new ArgumentException($"Provided file or folder path target {path} does not exist.");
+            bool hasSearchPattern = !string.IsNullOrEmpty(options.SearchPattern);
+            if (dirExists && !hasSearchPattern)
+                throw new ArgumentException($"Cannot compress target folder {path} without specifying a search pattern.");
+
+            if (fileExists)
+            {
+                LzUtility.CompressAvLzToDisk(path, options.AvGame, true);
+                Console.WriteLine($"Compressed: {path}");
+            }
+            else if (dirExists)
+            {
+                var searchOption = options.SearchSubdirectories ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+                var enumerable = LzUtility.CompressAvLzDirectoryToDisk(path, options.AvGame, true, searchOption, options.SearchPattern);
+                foreach (var file in enumerable)
+                    Console.WriteLine($"Compressed: {file.filePath}");
+            }
+            else
+            {
+                Assert.IsTrue(false, "This code should never run.");
             }
         }
 
