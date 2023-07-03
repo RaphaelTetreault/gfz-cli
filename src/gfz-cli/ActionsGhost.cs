6ab1eba2a8314071f2f0d16281c56f9b0909d026
Raphaël Tétreault <raphael.tetreault@outlook.com>
2023-07-03 00:38:10 -0400

    Add ability to read ghost data. TODO: make this useful ;)

--- src/gfz-cli/ActionsGhost.cs
+++ src/gfz-cli/ActionsGhost.cs
@@ -9,6 +9,7 @@ namespace Manifold.GFZCLI
     {
         public static void ReadGhosts(Options options)
         {
+            //string[] files = GetInputFiles(options);
             Terminal.WriteLine("Ghost: reading ghost data.");
             int binCount = DoFileInFileOutTasks(options, ReadGhost);
             Terminal.WriteLine($"Ghost: done reading {binCount} file{Plural(binCount)}.");
@@ -24,43 +25,12 @@ namespace Manifold.GFZCLI
                 ghost.FileName = Path.GetFileNameWithoutExtension(inputFile);
             }
 
+            // TODO: parameterize extensions
             outputFile.AppendExtension(".bin");
             var fileWrite = () =>
             {
-                // 0: ghost does not appear. Can win if time is better than timestamp in file.
-                // 1: timestamp seconds? When behind, estimate is borked AF.
-                // UNKNOWN
-                //ghost.Mutate(0, 0); // interpolation mode? 1-4
-                //ghost.Mutate(1, 0); // UNK lap data. Seems almost like "which lap", but 02 not on final dat
-
-                // ROTATION
-                //ghost.Mutate(2, 0);
-                //ghost.Mutate(3, 0);
-                //ghost.Mutate(4, 0);
-                //ghost.Mutate(5, 0);
-                //ghost.Mutate(6, 0);
-                //ghost.Mutate(7, 0);
-
-                // POSITION
-                //  8+ 9: pos x
-                // 10+11: pos y
-                // 12+13: pos z
-                //ghost.Mutate(8, 0);
-                //ghost.Mutate(9, 0);
-                //ghost.Mutate(10, 0);
-                //ghost.Mutate(11, 0);
-                //ghost.Mutate(12, 0);
-                //ghost.Mutate(13, 0);
-                
-                // INTERPOLATION - maybe fixed point duration?
-                //ghost.Mutate(14, 0);
-                //ghost.Mutate(15, 0);
-
-                using (var writer = new EndianBinaryWriter(File.Create(outputFile), GhostData.endianness))
-                {
-                    writer.Write(ghost);
-                }
-                File.Copy(outputFile, "D:\\boot-gfzj\\files\\staff_ghost\\" + outputFile.Name + ".dat", true);
+                using var writer = new EndianBinaryWriter(File.Create(outputFile), GhostData.endianness);
+                writer.Write(ghost);
             };
             var info = new FileWriteInfo()
             {
