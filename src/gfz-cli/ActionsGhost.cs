586648c047980cfde1e5bb167fcdc0f15548ae77
Raphaël Tétreault <raphael.tetreault@outlook.com>
2023-07-02 23:13:57 -0400

    m: WIP solve ghost data (use commit to easily edit data)

--- src/gfz-cli/ActionsGhost.cs
+++ src/gfz-cli/ActionsGhost.cs
@@ -27,8 +27,40 @@ namespace Manifold.GFZCLI
             outputFile.AppendExtension(".bin");
             var fileWrite = () =>
             {
-                using var writer = new EndianBinaryWriter(File.Create(outputFile), GhostData.endianness);
-                writer.Write(ghost);
+                // 0: ghost does not appear. Can win if time is better than timestamp in file.
+                // 1: timestamp seconds? When behind, estimate is borked AF.
+                // UNKNOWN
+                //ghost.Mutate(0, 0); // interpolation mode? 1-4
+                //ghost.Mutate(1, 0); // UNK lap data. Seems almost like "which lap", but 02 not on final dat
+
+                // ROTATION
+                //ghost.Mutate(2, 0);
+                //ghost.Mutate(3, 0);
+                //ghost.Mutate(4, 0);
+                //ghost.Mutate(5, 0);
+                //ghost.Mutate(6, 0);
+                //ghost.Mutate(7, 0);
+
+                // POSITION
+                //  8+ 9: pos x
+                // 10+11: pos y
+                // 12+13: pos z
+                //ghost.Mutate(8, 0);
+                //ghost.Mutate(9, 0);
+                //ghost.Mutate(10, 0);
+                //ghost.Mutate(11, 0);
+                //ghost.Mutate(12, 0);
+                //ghost.Mutate(13, 0);
+                
+                // INTERPOLATION - maybe fixed point duration?
+                //ghost.Mutate(14, 0);
+                //ghost.Mutate(15, 0);
+
+                using (var writer = new EndianBinaryWriter(File.Create(outputFile), GhostData.endianness))
+                {
+                    writer.Write(ghost);
+                }
+                File.Copy(outputFile, "D:\\boot-gfzj\\files\\staff_ghost\\" + outputFile.Name + ".dat", true);
             };
             var info = new FileWriteInfo()
             {
