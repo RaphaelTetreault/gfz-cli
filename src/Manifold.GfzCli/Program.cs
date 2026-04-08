cc04d03e247803221ba1b14e8301e4bdf78903bc
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2026-04-08 01:53:00 -0400

    Update project references and ensure build works properly

--- src/Manifold.GfzCli/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Manifold.GfzCli;
-public static class Program
-{
-    public static void Main(string[] args) => GfzCli.RunCliParseArgs(args);
-}
---
+++ src/gfz-cli/Program.cs
@@ -0,0 +1,6 @@
+﻿namespace GfzCliProgram;
+
+public static class Program
+{
+    public static void Main(string[] args) => Manifold.GfzCli.GfzCli.RunCliParseArgs(args);
+}
